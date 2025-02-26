4d12af707fa31ca3e4ef7a1833ba996ac1657ddd
acalic <acalic1990@gmail.com>
2025-02-26 19:27:31 +0000

    formatPrice function updated

--- frontend/src/lib/formatPrice.ts
+++ frontend/src/lib/formatPrice.ts
@@ -1,11 +1,26 @@
 export const formatPrice = (value: number): string => {
+  if (!value && value !== 0) return '--';
+
+  const trillion = 1_000_000_000_000;
+  const billion = 1_000_000_000;
+  const million = 1_000_000;
+  const thousand = 1_000;
+  const ten = 10;
+
   let formattedValue = '';
-  if (value >= 1_000_000) {
-    formattedValue = `${Math.round(value / 1_000_000)}M`;
-  } else if (value >= 1_000) {
-    formattedValue = `${Math.round(value / 1_000)}K`;
+  if (value >= trillion) {
+    formattedValue = `${(value / trillion).toFixed(1)}T`;
+  } else if (value >= billion) {
+    formattedValue = `${(value / billion).toFixed(0)}B`;
+  } else if (value >= million) {
+    formattedValue = `${(value / million).toFixed(0)}M`;
+  } else if (value >= thousand) {
+    formattedValue = `${(value / thousand).toFixed(0)}K`;
+  } else if (value < 10) {
+    formattedValue = value.toFixed(1);
   } else {
-    formattedValue = Math.round(value).toString();
+    formattedValue = value.toFixed(2);
   }
+
   return `$${formattedValue}`;
 };
